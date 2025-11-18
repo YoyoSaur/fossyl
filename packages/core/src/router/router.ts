4f596890a52c5c959c32e9804f15ca872b18db52
YoyoSaur <geverett94@gmail.com>
2025-11-18 09:57:08 -0500

    Better docs in the code, going to make an autogeneration of docs

--- packages/core/src/router/router.ts
+++ packages/core/src/router/router.ts
@@ -23,244 +23,179 @@ import { Params } from "./types/params.types";
 //
 
 function createEndpoint<Path extends string>(path: Path): Endpoint<Path> {
-  // Open route: no auth, no query
-  function get<Res extends unknown>(config: {
-    authenticator?: never;
-    queryValidator?: never;
-    handler: (params: { url: Params<Path> }) => Promise<Res>;
-  }): OpenRoute<Path, "GET", Res, undefined>;
+  function createNoBodyMethod<Method extends "GET" | "DELETE">(method: Method) {
+    function noBodyMethod<Res extends unknown>(config: {
+      authenticator?: never;
+      queryValidator?: never;
+      handler: (params: { url: Params<Path> }) => Promise<Res>;
+    }): OpenRoute<Path, Method, Res, undefined>;
 
-  // Open route: no auth, with query
-  function get<Res extends unknown, Query extends unknown>(config: {
-    authenticator?: never;
-    queryValidator: ValidatorFunction<Query>;
-    handler: (params: { url: Params<Path>; query: Query }) => Promise<Res>;
-  }): OpenRoute<Path, "GET", Res, Query>;
+    // Open route: no auth, with query
+    function noBodyMethod<Res extends unknown, Query extends unknown>(config: {
+      authenticator?: never;
+      queryValidator: ValidatorFunction<Query>;
+      handler: (params: { url: Params<Path>; query: Query }) => Promise<Res>;
+    }): OpenRoute<Path, Method, Res, Query>;
 
-  // Authenticated route: auth, no query
-  function get<Res extends unknown, Auth extends Authentication>(config: {
-    authenticator: AuthenticationFunction<Auth>;
-    queryValidator?: never;
-    handler: (params: { url: Params<Path> }, auth: Auth) => Promise<Res>;
-  }): AuthenticatedRoute<Path, "GET", Res, Auth, undefined>;
+    // Authenticated route: auth, no query
+    function noBodyMethod<Res extends unknown, Auth extends Authentication>(config: {
+      authenticator: AuthenticationFunction<Auth>;
+      queryValidator?: never;
+      handler: (params: { url: Params<Path> }, auth: Auth) => Promise<Res>;
+    }): AuthenticatedRoute<Path, Method, Res, Auth, undefined>;
 
-  // Authenticated route: auth, with query
-  function get<Res extends unknown, Auth extends Authentication, Query extends unknown>(config: {
-    authenticator: AuthenticationFunction<Auth>;
-    queryValidator: ValidatorFunction<Query>;
-    handler: (params: { url: Params<Path>; query: Query }, auth: Auth) => Promise<Res>;
-  }): AuthenticatedRoute<Path, "GET", Res, Auth, Query>;
+    // Authenticated route: auth, with query
+    function noBodyMethod<
+      Res extends unknown,
+      Auth extends Authentication,
+      Query extends unknown,
+    >(config: {
+      authenticator: AuthenticationFunction<Auth>;
+      queryValidator: ValidatorFunction<Query>;
+      handler: (params: { url: Params<Path>; query: Query }, auth: Auth) => Promise<Res>;
+    }): AuthenticatedRoute<Path, Method, Res, Auth, Query>;
 
-  function get(
-    config:
-      | { handler: (params: { url: Params<Path> }) => Promise<any> }
-      | { queryValidator: ValidatorFunction<any>; handler: (params: { url: Params<Path>; query: any }) => Promise<any> }
-      | { authenticator: AuthenticationFunction<any>; handler: (params: { url: Params<Path> }, auth: any) => Promise<any> }
-      | { authenticator: AuthenticationFunction<any>; queryValidator: ValidatorFunction<any>; handler: (params: { url: Params<Path>; query: any }, auth: any) => Promise<any> }
-  ): OpenRoute<Path, "GET", any, any> | AuthenticatedRoute<Path, "GET", any, any, any> {
-    if ("authenticator" in config) {
-      return { ...config, type: "authenticated", path, method: "GET" } satisfies AuthenticatedRoute<
-        Path,
-        "GET",
-        any,
-        any,
-        any
-      >;
-    } else {
-      return { ...config, type: "open", path, method: "GET" } satisfies OpenRoute<
-        Path,
-        "GET",
-        any,
-        any
-      >;
+    function noBodyMethod(
+      config:
+        | { handler: (params: { url: Params<Path> }) => Promise<any> }
+        | {
+            queryValidator: ValidatorFunction<any>;
+            handler: (params: { url: Params<Path>; query: any }) => Promise<any>;
+          }
+        | {
+            authenticator: AuthenticationFunction<any>;
+            handler: (params: { url: Params<Path> }, auth: any) => Promise<any>;
+          }
+        | {
+            authenticator: AuthenticationFunction<any>;
+            queryValidator: ValidatorFunction<any>;
+            handler: (params: { url: Params<Path>; query: any }, auth: any) => Promise<any>;
+          }
+    ): OpenRoute<Path, Method, any, any> | AuthenticatedRoute<Path, Method, any, any, any> {
+      if ("authenticator" in config) {
+        return {
+          ...config,
+          type: "authenticated",
+          path,
+          method,
+        } satisfies AuthenticatedRoute<Path, Method, any, any, any>;
+      } else {
+        return { ...config, type: "open", path, method } satisfies OpenRoute<
+          Path,
+          Method,
+          any,
+          any
+        >;
+      }
     }
+
+    return noBodyMethod;
   }
 
-  // Validated route: no auth, no query
-  function put<Res extends unknown, RequestBody extends unknown>(config: {
-    authenticator?: never;
-    validator: ValidatorFunction<RequestBody>;
-    queryValidator?: never;
-    handler: (params: { url: Params<Path> }, body: RequestBody) => Promise<Res>;
-  }): ValidatedRoute<Path, "PUT", Res, RequestBody, undefined>;
+  function createBodyMethod<Method extends "POST" | "PUT">(method: Method) {
+    // Validated route: no auth, no query
+    function bodyMethod<Res extends unknown, RequestBody extends unknown>(config: {
+      authenticator?: never;
+      validator: ValidatorFunction<RequestBody>;
+      queryValidator?: never;
+      handler: (params: { url: Params<Path> }, body: RequestBody) => Promise<Res>;
+    }): ValidatedRoute<Path, Method, Res, RequestBody, undefined>;
 
-  // Validated route: no auth, with query
-  function put<Res extends unknown, RequestBody extends unknown, Query extends unknown>(config: {
-    authenticator?: never;
-    validator: ValidatorFunction<RequestBody>;
-    queryValidator: ValidatorFunction<Query>;
-    handler: (params: { url: Params<Path>; query: Query }, body: RequestBody) => Promise<Res>;
-  }): ValidatedRoute<Path, "PUT", Res, RequestBody, Query>;
+    // Validated route: no auth, with query
+    function bodyMethod<
+      Res extends unknown,
+      RequestBody extends unknown,
+      Query extends unknown,
+    >(config: {
+      authenticator?: never;
+      validator: ValidatorFunction<RequestBody>;
+      queryValidator: ValidatorFunction<Query>;
+      handler: (params: { url: Params<Path>; query: Query }, body: RequestBody) => Promise<Res>;
+    }): ValidatedRoute<Path, Method, Res, RequestBody, Query>;
 
-  // Full route: auth + body validation, no query
-  function put<Res extends unknown, RequestBody extends unknown, Auth extends Authentication>(config: {
-    authenticator: AuthenticationFunction<Auth>;
-    validator: ValidatorFunction<RequestBody>;
-    queryValidator?: never;
-    handler: (params: { url: Params<Path> }, auth: Auth, body: RequestBody) => Promise<Res>;
-  }): FullRoute<Path, "PUT", Res, RequestBody, Auth, undefined>;
+    // Full route: auth + body validation, no query
+    function bodyMethod<
+      Res extends unknown,
+      RequestBody extends unknown,
+      Auth extends Authentication,
+    >(config: {
+      authenticator: AuthenticationFunction<Auth>;
+      validator: ValidatorFunction<RequestBody>;
+      queryValidator?: never;
+      handler: (params: { url: Params<Path> }, auth: Auth, body: RequestBody) => Promise<Res>;
+    }): FullRoute<Path, Method, Res, RequestBody, Auth, undefined>;
 
-  // Full route: auth + body validation, with query
-  function put<
-    Res extends unknown,
-    RequestBody extends unknown,
-    Auth extends Authentication,
-    Query extends unknown,
-  >(config: {
-    authenticator: AuthenticationFunction<Auth>;
-    validator: ValidatorFunction<RequestBody>;
-    queryValidator: ValidatorFunction<Query>;
-    handler: (params: { url: Params<Path>; query: Query }, auth: Auth, body: RequestBody) => Promise<Res>;
-  }): FullRoute<Path, "PUT", Res, RequestBody, Auth, Query>;
+    // Full route: auth + body validation, with query
+    function bodyMethod<
+      Res extends unknown,
+      RequestBody extends unknown,
+      Auth extends Authentication,
+      Query extends unknown,
+    >(config: {
+      authenticator: AuthenticationFunction<Auth>;
+      validator: ValidatorFunction<RequestBody>;
+      queryValidator: ValidatorFunction<Query>;
+      handler: (
+        params: { url: Params<Path>; query: Query },
+        auth: Auth,
+        body: RequestBody
+      ) => Promise<Res>;
+    }): FullRoute<Path, Method, Res, RequestBody, Auth, Query>;
 
-  function put(
-    config:
-      | { validator: ValidatorFunction<any>; handler: (params: { url: Params<Path> }, body: any) => Promise<any> }
-      | { validator: ValidatorFunction<any>; queryValidator: ValidatorFunction<any>; handler: (params: { url: Params<Path>; query: any }, body: any) => Promise<any> }
-      | { authenticator: AuthenticationFunction<any>; validator: ValidatorFunction<any>; handler: (params: { url: Params<Path> }, auth: any, body: any) => Promise<any> }
-      | { authenticator: AuthenticationFunction<any>; validator: ValidatorFunction<any>; queryValidator: ValidatorFunction<any>; handler: (params: { url: Params<Path>; query: any }, auth: any, body: any) => Promise<any> }
-  ): ValidatedRoute<Path, "PUT", any, any, any> | FullRoute<Path, "PUT", any, any, any, any> {
-    if ("authenticator" in config) {
-      return { ...config, type: "full", path, method: "PUT" } satisfies FullRoute<
-        Path,
-        "PUT",
-        any,
-        any,
-        any,
-        any
-      >;
-    } else {
-      return { ...config, type: "validated", path, method: "PUT" } satisfies ValidatedRoute<
-        Path,
-        "PUT",
-        any,
-        any,
-        any
-      >;
+    function bodyMethod(
+      config:
+        | {
+            validator: ValidatorFunction<any>;
+            handler: (params: { url: Params<Path> }, body: any) => Promise<any>;
+          }
+        | {
+            validator: ValidatorFunction<any>;
+            queryValidator: ValidatorFunction<any>;
+            handler: (params: { url: Params<Path>; query: any }, body: any) => Promise<any>;
+          }
+        | {
+            authenticator: AuthenticationFunction<any>;
+            validator: ValidatorFunction<any>;
+            handler: (params: { url: Params<Path> }, auth: any, body: any) => Promise<any>;
+          }
+        | {
+            authenticator: AuthenticationFunction<any>;
+            validator: ValidatorFunction<any>;
+            queryValidator: ValidatorFunction<any>;
+            handler: (
+              params: { url: Params<Path>; query: any },
+              auth: any,
+              body: any
+            ) => Promise<any>;
+          }
+    ): ValidatedRoute<Path, Method, any, any, any> | FullRoute<Path, Method, any, any, any, any> {
+      if ("authenticator" in config) {
+        return { ...config, type: "full", path, method } satisfies FullRoute<
+          Path,
+          Method,
+          any,
+          any,
+          any,
+          any
+        >;
+      } else {
+        return { ...config, type: "validated", path, method } satisfies ValidatedRoute<
+          Path,
+          Method,
+          any,
+          any,
+          any
+        >;
+      }
     }
-  }
-
-  // Validated route: no auth, no query
-  function post<Res extends unknown, RequestBody extends unknown>(config: {
-    authenticator?: never;
-    validator: ValidatorFunction<RequestBody>;
-    queryValidator?: never;
-    handler: (params: { url: Params<Path> }, body: RequestBody) => Promise<Res>;
-  }): ValidatedRoute<Path, "POST", Res, RequestBody, undefined>;
-
-  // Validated route: no auth, with query
-  function post<Res extends unknown, RequestBody extends unknown, Query extends unknown>(config: {
-    authenticator?: never;
-    validator: ValidatorFunction<RequestBody>;
-    queryValidator: ValidatorFunction<Query>;
-    handler: (params: { url: Params<Path>; query: Query }, body: RequestBody) => Promise<Res>;
-  }): ValidatedRoute<Path, "POST", Res, RequestBody, Query>;
-
-  // Full route: auth + body validation, no query
-  function post<Res extends unknown, RequestBody extends unknown, Auth extends Authentication>(config: {
-    authenticator: AuthenticationFunction<Auth>;
-    validator: ValidatorFunction<RequestBody>;
-    queryValidator?: never;
-    handler: (params: { url: Params<Path> }, auth: Auth, body: RequestBody) => Promise<Res>;
-  }): FullRoute<Path, "POST", Res, RequestBody, Auth, undefined>;
-
-  // Full route: auth + body validation, with query
-  function post<
-    Res extends unknown,
-    RequestBody extends unknown,
-    Auth extends Authentication,
-    Query extends unknown,
-  >(config: {
-    authenticator: AuthenticationFunction<Auth>;
-    validator: ValidatorFunction<RequestBody>;
-    queryValidator: ValidatorFunction<Query>;
-    handler: (params: { url: Params<Path>; query: Query }, auth: Auth, body: RequestBody) => Promise<Res>;
-  }): FullRoute<Path, "POST", Res, RequestBody, Auth, Query>;
-
-  function post(
-    config:
-      | { validator: ValidatorFunction<any>; handler: (params: { url: Params<Path> }, body: any) => Promise<any> }
-      | { validator: ValidatorFunction<any>; queryValidator: ValidatorFunction<any>; handler: (params: { url: Params<Path>; query: any }, body: any) => Promise<any> }
-      | { authenticator: AuthenticationFunction<any>; validator: ValidatorFunction<any>; handler: (params: { url: Params<Path> }, auth: any, body: any) => Promise<any> }
-      | { authenticator: AuthenticationFunction<any>; validator: ValidatorFunction<any>; queryValidator: ValidatorFunction<any>; handler: (params: { url: Params<Path>; query: any }, auth: any, body: any) => Promise<any> }
-  ): ValidatedRoute<Path, "POST", any, any, any> | FullRoute<Path, "POST", any, any, any, any> {
-    if ("authenticator" in config) {
-      return { ...config, type: "full", path, method: "POST" } satisfies FullRoute<
-        Path,
-        "POST",
-        any,
-        any,
-        any,
-        any
-      >;
-    } else {
-      return { ...config, type: "validated", path, method: "POST" } satisfies ValidatedRoute<
-        Path,
-        "POST",
-        any,
-        any,
-        any
-      >;
-    }
-  }
-
-  // Open route: no auth, no query
-  function del<Res extends unknown>(config: {
-    authenticator?: never;
-    queryValidator?: never;
-    handler: (params: { url: Params<Path> }) => Promise<Res>;
-  }): OpenRoute<Path, "DELETE", Res, undefined>;
-
-  // Open route: no auth, with query
-  function del<Res extends unknown, Query extends unknown>(config: {
-    authenticator?: never;
-    queryValidator: ValidatorFunction<Query>;
-    handler: (params: { url: Params<Path>; query: Query }) => Promise<Res>;
-  }): OpenRoute<Path, "DELETE", Res, Query>;
-
-  // Authenticated route: auth, no query
-  function del<Res extends unknown, Auth extends Authentication>(config: {
-    authenticator: AuthenticationFunction<Auth>;
-    queryValidator?: never;
-    handler: (params: { url: Params<Path> }, auth: Auth) => Promise<Res>;
-  }): AuthenticatedRoute<Path, "DELETE", Res, Auth, undefined>;
-
-  // Authenticated route: auth, with query
-  function del<Res extends unknown, Auth extends Authentication, Query extends unknown>(config: {
-    authenticator: AuthenticationFunction<Auth>;
-    queryValidator: ValidatorFunction<Query>;
-    handler: (params: { url: Params<Path>; query: Query }, auth: Auth) => Promise<Res>;
-  }): AuthenticatedRoute<Path, "DELETE", Res, Auth, Query>;
-
-  function del(
-    config:
-      | { handler: (params: { url: Params<Path> }) => Promise<any> }
-      | { queryValidator: ValidatorFunction<any>; handler: (params: { url: Params<Path>; query: any }) => Promise<any> }
-      | { authenticator: AuthenticationFunction<any>; handler: (params: { url: Params<Path> }, auth: any) => Promise<any> }
-      | { authenticator: AuthenticationFunction<any>; queryValidator: ValidatorFunction<any>; handler: (params: { url: Params<Path>; query: any }, auth: any) => Promise<any> }
-  ): OpenRoute<Path, "DELETE", any, any> | AuthenticatedRoute<Path, "DELETE", any, any, any> {
-    if ("authenticator" in config) {
-      return {
-        ...config,
-        type: "authenticated",
-        path,
-        method: "DELETE",
-      } satisfies AuthenticatedRoute<Path, "DELETE", any, any, any>;
-    } else {
-      return { ...config, type: "open", path, method: "DELETE" } satisfies OpenRoute<
-        Path,
-        "DELETE",
-        any,
-        any
-      >;
-    }
+    return bodyMethod;
   }
 
   return {
-    get,
-    post,
-    put,
-    delete: del,
+    get: createNoBodyMethod("GET"),
+    post: createBodyMethod("POST"),
+    put: createBodyMethod("PUT"),
+    delete: createNoBodyMethod("DELETE"),
   };
 }
 
